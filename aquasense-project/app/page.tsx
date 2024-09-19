129266eb46b0019d40bebb5a9910fc2aae12b81e
student <daisychepkirui@gmail.com>
2024-09-19 13:42:57 +0300

    added team

--- aquasense-project/app/page.tsx
+++ aquasense-project/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-
+import Footer from "./Footer";
+import Team from "./Team";
 export default function Home() {
   return (
     <div className="">
-      <main className="">
-        
+      <main>
+        <Team/>
+        <Footer/>
+       
       </main>
       
     </div>

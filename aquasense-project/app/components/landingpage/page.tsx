62ce0fc7bf734a117d12329b8b3aae09b9e4add7
Mutesi-alin <alinemutesiii@gmail.com>
2024-09-19 14:08:37 +0300

    add file

---
+++ aquasense-project/app/components/landingpage/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+
+const LandingPage = () => {
+  return (
+    <div className="relative h-[calc(100vh-4.5rem)]">
+      <Image
+        src="/landingpage.png"
+        alt="Drainage system"
+        layout="fill"
+        objectFit="cover"
+        className="brightness-100"
+      />
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-4 text-center">
+        <h1 className="text-3xl md:text-8xl font-bold mb-4 text-blue-600">Blockage Buster</h1>
+        <p className="text-lg md:text-2xl mb-6 font-bold">Say goodbye to drainage disasters with our smart solution!</p>
+        <button className="bg-blue-600 hover:bg-blue-1000 text-white font-bold py-2 px-6 rounded-full text-base">
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
--- aquasense-project/app/page.tsx
+++ aquasense-project/app/page.tsx
@@ -1,12 +1,19 @@
-import Image from "next/image";
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './components/navbar';
+import LandingPage from './components/landingpage/page';
 
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
-        
-      </main>
-      
+    <div>
+      <Head>
+        <title>Aquasense - Smart Drainage System</title>
+        <meta name="description" content="Say goodbye to drainage disasters with our smart solution!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <LandingPage />
     </div>
   );
 }

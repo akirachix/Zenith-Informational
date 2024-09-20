944764b1642835bd05bcb47ceb4f5b49cf942f84
student <daisychepkirui@gmail.com>
2024-09-20 17:38:12 +0300

    add file

--- aquasense-project/app/components/landingpage/indext.tsx
+++ aquasense-project/app/components/landingpage/indext.tsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
   return (
     <div  id="home"className="relative h-[calc(100vh-4.5rem)] font-serif">
       <Image
-        src="/landingpage.png"
+        src="/images/landingpage.png"
         alt="Drainage system"
         layout="fill"
         objectFit="cover"

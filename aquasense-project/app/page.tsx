6d358d086cbf5116ca4ab27d5600486c78d56746
nyabangWech <nyabangwech@gmail.com>
2024-09-20 09:46:40 +0300

    added aboutUs section

--- aquasense-project/app/page.tsx
+++ aquasense-project/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+import AquaSenseAbout from "./About Us";
 
 export default function Home() {
   return (
     <div className="">
-      <main className="">
+      <main>
+        
+        <AquaSenseAbout/>
         
       </main>
       
